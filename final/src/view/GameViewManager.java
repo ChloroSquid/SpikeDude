ea73775867f3d93b63a07f772d927e7517fc1853
coreyblanke <car4corey@gmail.com>
2020-04-28 23:56:46 -0500

    Skeleton of the game view

---
+++ final/src/view/GameViewManager.java
@@ -0,0 +1,60 @@
+package view;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class GameViewManager {
+	private AnchorPane gamePane;
+	private Scene gameScene;
+	private Stage gameStage;
+	
+	private static final int GAME_WIDTH = 600;
+	private static final int GAME_HEIGHT = 600;
+	
+	public GameViewManager() {
+		initializeStage();
+		createKeyListeners();
+	}
+	
+	private void initializeStage() {
+		gamePane = new AnchorPane();
+		gameScene = new Scene(gamePane, GAME_WIDTH, GAME_HEIGHT);
+		gameStage = new Stage();
+		gameStage.setScene(gameScene);
+		
+	}
+	
+	private void createKeyListeners() {
+		gameScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			
+			@Override
+			public void handle(KeyEvent event) {
+				//todo finish handling
+				switch (event.getCode()){
+					case RIGHT:
+						break;
+					case LEFT:
+						break;
+					case UP:
+						break;
+					default:
+						break;
+				}
+			}
+			
+		});
+		
+		gameScene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			
+			@Override
+			public void handle(KeyEvent event) {
+				
+			}
+			
+		});
+	}
+}
